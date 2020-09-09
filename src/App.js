6905f628ae7d49862f2423c0f339f522f429e912
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2020-09-09 17:52:09 +0530

    Added amazon-clone header bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';    // importing header from header.js file
 
 function App() {
   return (
     // BEM
     <div className="App">
-      <h1>Hello Clever Programmers, let's bulid the Amazon Store!</h1>
+      {/* <h1>Hello Clever Programmers, let's bulid the Amazon Store!</h1> */}
+
+      {/* Header */}
+      <Header />
+
+      {/* Home */}
     </div>
   );
 }
--- src/firebase-demo.js
+++ src/firebase-demo.js
@@ -1,3 +1,6 @@
+// you need to go firebase.google.com --> console --> create new app
+// go to new app settings and copy settings as config 
+// once done create firebase.js in src/ directory --> paste and save them.
 const firebaseConfig = {
     apiKey: "",
     authDomain: "",
