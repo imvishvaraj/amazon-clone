20391baff9c40a9c3b7d58faa9755a308972fbd4
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2020-09-09 20:28:49 +0530

    dynamic product information added.

--- src/Product.js
+++ src/Product.js
@@ -1,22 +1,26 @@
 import React from 'react'; 
 import "./Product.css";
+import StarIcon from '@material-ui/icons/Star';
 
-function Product() {
+function Product({title, image, price, rating}) {
     return (
         <div className="product">
             <div className="product__info">
-                <p>The Lean Startup</p>
+                <p>{title}</p>
                 <p className="product__price">
                     <small>$</small>
-                    <strong>19.99</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product__rating">
-                    <p>*</p>
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) =>(
+                            <p><StarIcon /></p>
+                        ))}
                 </div>
             </div>
 
-            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" alt="" />
-
+            <img src={image} alt="" />
             <button>Add to Basket</button>
         </div>
     )
