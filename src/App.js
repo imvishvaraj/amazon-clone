ba5fcd8fd2b275f8082df3f15ed4f5a4d75a67ae
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2020-09-10 00:39:36 +0530

    Added route, subtotal, clickable links, added dynamic items count

--- src/App.js
+++ src/App.js
@@ -2,19 +2,38 @@ import React from 'react';
 import './App.css';
 import Header from './Header';    // importing header from header.js file
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from './Checkout';
 
 function App() {
   return (
     // BEM
-    <div className="App">
+    <Router>
+      <div className="App">
       {/* <h1>Hello Clever Programmers, let's bulid the Amazon Store!</h1> */}
+      <Header/>
 
-      {/* Header */}
-      <Header />
+      <Switch>
+        <Route path="/checkout">
+            
+            <Checkout/>
+        
+        </Route>
 
-      {/* Home */}
-      <Home />
+        {/* Note: always keep main route component at bottom else other route will not get hit! */}
+        <Route path="/">
+            {/* Adding Header main page. */}
+
+
+            {/* Adding Home content into main page. */}
+            <Home/>
+        </Route>
+
+      </Switch>   
+
     </div>
+    </Router>
+    
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,19 @@ import React from 'react';
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasket from "@material-ui/icons/ShoppingBasket";
+import { Link } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
 
 function Header() {
+    const [{ basket }, dispatch] = useStateValue();
+    
     return (
         <div className="header">
-            {/* Logo http://pngimg.com/uploads/amazon/amazon_PNG11.png */}
-            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+            <Link to="/">
+                <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+            </Link>
+            {/* Logo  */}
+            
 
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
@@ -30,10 +37,15 @@ function Header() {
                     <span className="header__optionLineTwo">Prime</span>
                 </div>
 
-                <div className="header__optionBasket">
-                    <ShoppingBasket />
-                    <span className="header__optionLineTwo header__basketCount">0</span>
-                </div>
+                <Link to="/checkout">
+                    <div className="header__optionBasket">
+                        <ShoppingBasket />
+                        <span className="header__optionLineTwo header__basketCount">
+                            {basket?.length}
+                        </span>
+                    </div>
+                </Link>
+                
             </div>
 
         </div>
