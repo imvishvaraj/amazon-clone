d9b4e60b14e2644c7b30d207a80088be3d7a52ad
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2020-09-11 00:44:39 +0530

    login front end design

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Header';    // importing header from header.js file
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from './Checkout';
+import Login from'./Login';
 
 function App() {
   return (
@@ -11,20 +12,22 @@ function App() {
     <Router>
       <div className="App">
       {/* <h1>Hello Clever Programmers, let's bulid the Amazon Store!</h1> */}
-      <Header/>
 
       <Switch>
-        <Route path="/checkout">
-            
-            <Checkout/>
         
+        <Route path="/login">
+            <Login />
+        </Route>
+        
+        <Route path="/checkout">
+            <Header/>
+            <Checkout/>
         </Route>
 
         {/* Note: always keep main route component at bottom else other route will not get hit! */}
+
         <Route path="/">
-            {/* Adding Header main page. */}
-
-
+            <Header/>
             {/* Adding Home content into main page. */}
             <Home/>
         </Route>
