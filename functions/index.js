7607ff3b97c328af1e5b48353afe5f2329267321
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2020-09-13 23:55:44 +0530

    added backend with expressjs for payment processing.

--- functions/index.js
+++ functions/index.js
@@ -14,10 +14,35 @@ const stripe = require("stripe")('sk_test_51HQdHEC8SGpPYV1LZG3AtV49X5axr2j0ACvYp
 // API
 
 // App config
+const app = express();
 
 // Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
 
 // API Routes
+app.get('/', (request, response) => response.status(200).send('hello world'))
+
+app.post('/payments/create', async (request, response) => {
+    const total = request.query.total;
+
+    console.log('Payment Request Received BOOM!!! for this amount >>> ', total)
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,  // subunits of the currency
+        currency: "inr",
+    });
+
+    // ok - created
+    console.log(paymentIntent)
+    console.log('client_secret >>> ', paymentIntent.client_secret)
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret
+    })
+})
 
 // Listen command
+exports.api = functions.https.onRequest(app)
 
+// Example End Point 
+// http://localhost:5001/clone-7261a/us-central1/api
