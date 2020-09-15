63ced94e989fb4c90f26328cec5fbd97eaf0a015
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2020-09-16 00:57:47 +0530

    minor changes

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,9 @@
 const functions = require('firebase-functions');
 const express = require("express"); 
 const cors = require("cors");
-const stripe = require("stripe")('sk_test_51HQdHEC8SGpPYV1LZG3AtV49X5axr2j0ACvYpoTgBXTp2wrx5Ol8zHUm5ZiYU3LmPwkUWwEMe0zT4mKlXV3i27uN00MyJvMzm4');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// go to your stripe account and get stripe secret key
+const stripe = require("stripe")('sk_test_51HQdHEC8SGpPYV1LZG3AtV49X5axr2j0ACvYpoTgBXTp2wrx5Ol8zHUm5ZiYU3LmPwkUWwEMe0zT4mKlXV3i27uN00MyJvMzm4');
 
 // API
 
--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
+    // deploy function using `firebase deploy --only functions`
+    // and use that url for processing
     baseURL: 'http://localhost:5001/clone-7261a/us-central1/api'      // The API (cloud function) URL
 });
 
